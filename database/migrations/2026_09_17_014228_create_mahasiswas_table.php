e83c0e32b0d4cc81c5de1d55d452b9ad807e7e18
saintnetarf <saintnetarf@gmail.com>
2026-09-17 09:49:58 +0800

    tambahan mahasiswa

---
+++ database/migrations/2026_09_17_014228_create_mahasiswas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('nim');
+            $table->string('prodi');
+            $table->string('angkatan');
+            $table->string('jenis_kelamin');
+            $table->string('email');
+            $table->string('alamat');
+            $table->string('no_telepon');
+            $table->string('no_ktp');
+            $table->string('foto_profil');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+};
